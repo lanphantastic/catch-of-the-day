bf892ad07df16775183c82c4b8b831313ad6ce9d
Lan Phan <lanc_phan@hotmail.com>
2018-01-10 17:51:12 -0500

    example of using comment for js and jsx

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
+    // Any where else. this is regular comment for js
     return (
       <form className="store-selector">
+      { /* This is a comment for jsx. Can only be inside a parent. Also, the return can ONLY return one parent. If you want to use two, wrap everything inside under a div */}
       <h2>Please Enter A Store</h2>
       <input type="text" required placeholder="Store Name" />
       <button type="Submit">Vist Store</button>
