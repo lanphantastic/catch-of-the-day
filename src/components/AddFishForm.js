12a5254e0731ca55e9937d2287aa02ce39aab746
Lan Phan <lanc_phan@hotmail.com>
2018-01-22 16:46:16 -0500

    add a method (AddEventListener equivalent) on a submit click and prevent the click's default behaviour

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+  createFish(event){
+    event.preventDefault();
+    console.log('Gonna make some fish! ');
+  }
+
   render() {
     return (
-      <form className="fish-edit">
+      <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
         <input type="text" placeholder="Fish Name" />
         <input type="text" placeholder="Fish Price" />
         <select>
