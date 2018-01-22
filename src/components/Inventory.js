69277f7dd62fc3909628e0f45d2f7c1ab725d034
Lan Phan <lanc_phan@hotmail.com>
2018-01-22 16:39:29 -0500

    add a fish form component

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,21 @@
 import React from 'react';
+// import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
     return (
-      <p>Inventory</p>
+      <form className="fish-edit">
+        <input type="text" placeholder="Fish Name" />
+        <input type="text" placeholder="Fish Price" />
+        <select>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea placeholder="Fish Desc"></textarea>
+        <input type="text" placeholder="Fish Image"/>
+        <button type="submit">+ Add Item</button>
+      </form>
+
     )
   }
 }
