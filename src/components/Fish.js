ed5ccb4a6f0505f2c52ea18ff43a6d435ca8ff56
Lan Phan <lanc_phan@hotmail.com>
2018-01-23 14:10:15 -0500

    Convert the object into an array we can loop through each fish. And then append the Fish component to see the picture and name

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Fish extends React.Component {
   render(){
     return(
-      <li className="menu-fish"></li>
+      <li className="menu-fish">
+      <img src={this.props.details.image} alt={this.props.details.name}/>
+      {this.props.details.name}
+      </li>
     )
   }
 }
