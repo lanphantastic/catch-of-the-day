3c9acb963cb092a111e3cb0c66200c079fac010e
Lan Phan <lanc_phan@hotmail.com>
2018-01-23 13:42:19 -0500

    import sample-fishes.js, make a loadSamples function to set the fish state to the sample fishes. Then bind it so it can be access from the constructor and pass it down to the inventory component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
 
@@ -9,6 +10,7 @@ class App extends React.Component {
   constructor(){
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     // get initial state
     this.state = {
       fishes: {},
@@ -29,6 +31,11 @@ class App extends React.Component {
     this.setState({ fishes }); // alternative: fishes: fishes (line 13: line 22)
   }
 
+  loadSamples(){
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -36,7 +43,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
