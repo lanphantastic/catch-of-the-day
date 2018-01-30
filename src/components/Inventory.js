d00c29e58ebb3fb602861bf6459212f92412cefa
Lan Phan <lanc_phan@hotmail.com>
2018-01-30 11:03:18 -0500

    created a method to render the login social media account button

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -6,6 +6,12 @@ class Inventory extends React.Component {
     super();
     this.renderInventory = this.renderInventory.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.renderLogin = this.renderLogin.bind(this);
+
+    this.state = {
+      uid: null,
+      owner: null
+    }
   }
 
   handleChange(e, key) {
@@ -16,6 +22,19 @@ class Inventory extends React.Component {
       [e.target.name]: e.target.value
     }
     this.props.updateFish(key, updatedFish);
+  }
+
+  renderLogin(){
+    return(
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={() => this.authenticate('github')}>Log In with Github</button>
+        <button className="facebook" onClick={() => this.authenticate('facebook')}>Log In with Facebook</button>
+        <button className="twitter" onClick={() => this.authenticate('twitter')}>Log In with Twitter</button>
+
+      </nav>
+    )
   }
 
   renderInventory(key) {
@@ -38,6 +57,10 @@ class Inventory extends React.Component {
   }
 
   render() {
+    // check if anyone is login at all
+    if (!this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
     return (
       <div>
         <h2>Inventory</h2>
